c6e307053fc90e2253b85d9bd5ae493de430592d
qinyibei <qinyibei@gmail.com>
2017-03-26 01:27:03 +0800

    trackpoint ok

--- TrackPoint_INT.ino
+++ TrackPoint_INT.ino
@@ -7,11 +7,12 @@
  * Pin 2			: DATA
  * Pin 4			: RESET
  */
+#include <Mouse.h>
 #include "TrackPoint.h"
 
-#define	CLOCK		3
 #define DATA		2
-#define RESET		0
+#define	CLOCK		3
+#define RESET		4
 #define CLOCK_INT	0
 
 TrackPoint trackpoint(CLOCK, DATA, RESET, true);
@@ -19,21 +20,35 @@ TrackPoint trackpoint(CLOCK, DATA, RESET, true);
 void setup()
 {	
 	Mouse.begin();
+	pinMode(LED_BUILTIN, OUTPUT);
 
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(1000);
 	trackpoint.reset();
-	trackpoint.setSensitivityFactor(0xC0);
-	trackpoint.setStreamMode();
+	digitalWrite(LED_BUILTIN, LOW);
 
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(1000);
+	trackpoint.setSensitivityFactor(0xC0);
+	digitalWrite(LED_BUILTIN, LOW);
+
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(1000);
+	trackpoint.setStreamMode();
+	digitalWrite(LED_BUILTIN, LOW);
+
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(1000);
 	attachInterrupt(CLOCK_INT, clockInterrupt, FALLING);
+	digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop()
 {	
 	if(trackpoint.reportAvailable()) {
 		TrackPoint::DataReport d = trackpoint.getStreamReport();
-		Mouse.move(d.x, -d.y, 0);
+		Mouse.move(-d.x, d.y, 0);
 	} 
-	
 }
 
 void clockInterrupt(void) {
